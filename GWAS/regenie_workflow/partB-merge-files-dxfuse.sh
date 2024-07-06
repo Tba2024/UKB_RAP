8b09e4ef6471fcd19b8bbe2ab20e707e9e6b9d67
Tba2024 <162927760+Tba2024@users.noreply.github.com>
2024-07-07 01:24:55 +1000

    Update partB-merge-files-dxfuse.sh

--- GWAS/regenie_workflow/partB-merge-files-dxfuse.sh
+++ GWAS/regenie_workflow/partB-merge-files-dxfuse.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
+set -e
+set -x
 # Requirements: 
 # Please refer to readme.md for more information about the requirements
 
@@ -15,12 +16,14 @@
 # - ukb22418_c1_22_v2_merged.fam - used as input for parts C and D
 
 #cmd to run (use as input with `-icmd=$run_merge`)
+
+echo "first code"
 run_merge="cp /mnt/project/Bulk/Genotype\ Results/Genotype\ calls/ukb22418_c[1-9].* /mnt/project/Bulk/Genotype\ Results/Genotype\ calls/ukb22418_c1[0-9].* /mnt/project/Bulk/Genotype\ Results/Genotype\ calls/ukb22418_c2[0-2].* . ;\
         ls *.bed | sed -e 's/.bed//g' > files_to_merge.txt; \
         plink --merge-list files_to_merge.txt --make-bed\
         --autosome-xy --out ukb22418_c1_22_v2_merged; \
         rm files_to_merge.txt;"
-
+echo "second code"
 dx run swiss-army-knife -iin="/Data/main_wes_450k.phe" \
    -icmd="${run_merge}" --tag="Step1" --instance-type "mem1_ssd1_v2_x16"\
    --destination="/Data/" --brief --yes
