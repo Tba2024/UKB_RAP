e1efaf2328422bf996576460cadb85a8419b21de
Tba2024 <162927760+Tba2024@users.noreply.github.com>
2024-07-07 21:53:13 +1000

    Update partC-step1-qc-filter.sh

--- GWAS/regenie_workflow/partC-step1-qc-filter.sh
+++ GWAS/regenie_workflow/partC-step1-qc-filter.sh
@@ -24,7 +24,7 @@ data_field="22418"
 data_file_dir="/Data/"
 
 run_plink_qc="plink2 --bfile ukb${data_field}_c1_22_v1_merged\
- --keep diabetes_wes_200k.phe --autosome\
+ --keep main_wes_450k.phe --autosome\
  --maf 0.01 --mac 20 --geno 0.1 --hwe 1e-15\
  --mind 0.1 --write-snplist --write-samples\
  --no-id-header --out  WES_array_snps_qc_pass"
@@ -32,6 +32,6 @@ run_plink_qc="plink2 --bfile ukb${data_field}_c1_22_v1_merged\
 dx run swiss-army-knife -iin="${data_file_dir}/ukb${data_field}_c1_22_v1_merged.bed" \
    -iin="${data_file_dir}/ukb${data_field}_c1_22_v1_merged.bim" \
    -iin="${data_file_dir}/ukb${data_field}_c1_22_v1_merged.fam"\
-   -iin="${data_file_dir}/diabetes_wes_200k.phe" \
+   -iin="${data_file_dir}/main_wes_450k.phe" \
    -icmd="${run_plink_qc}" --tag="Step1" --instance-type "mem1_ssd1_v2_x16"\
    --destination="${project}:/Data/" --brief --yes
